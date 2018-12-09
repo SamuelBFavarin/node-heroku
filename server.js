c4b4a9582f00df61f0d0cbf2edbdf6fde5106108
SamuelBFavarin <samuelbfavarin@hotmail.com>
2018-12-09 21:46:26 -0200

    finalizacao frontend

--- server.js
+++ server.js
@@ -3,16 +3,16 @@ var express = require('express');
 var app = express();
 var port = process.env.PORT || 3000;
 
-
+// base de dados :)
 var data = [
     {
       "id": 0,
-      "title" : "Dar uma volta com a gatinha",
+      "title" : "Fazer trabalho de Sistemas Distribuídos",
       "deleted" : 0
     },
     {
       "id": 1,
-      "title" : "Dar um pedala robinho",
+      "title" : "Estudar para prova",
       "deleted" : 0
     }
 ]
@@ -27,6 +27,7 @@ app.get("/list", function(req,res){
     res.send(data)
 });
 
+// adiciona dados a base
 app.get("/add", function(req,res){
     var title = req.query.title;
     var id = data.length + 1
@@ -39,9 +40,9 @@ app.get("/add", function(req,res){
     res.send(data)
 });
 
+// remove dados da base
 app.get("/remove", function(req,res){
     var id = req.query.id;
-    // data = JSON.parse(JSON.stringify(data))
     for (var i = 0; i<data.length; i++){
         if (data[i].id == id){
             data[i].deleted = 1

c08fdbc3809c2ec08f9d096ea756f7a303162b9b
SamuelBFavarin <samuelbfavarin@hotmail.com>
2018-12-09 20:02:12 -0200

    incluindo vue.js

--- server.js
+++ server.js
@@ -3,9 +3,21 @@ var express = require('express');
 var app = express();
 var port = process.env.PORT || 3000;
 
+// rota para view
 app.get("/", function(req,res){
-    res.send("Samuel On Heroku")
+    res.sendfile('index.html')
 });
+
+// rota para funcoes
+app.get("/list", function(req,res){
+    res.sendfile('data.json')
+});
+
+app.get("/add", function(req,res){
+    console.log('aqui')
+    res.send("add")
+});
+
 
 app.listen(port)
 
